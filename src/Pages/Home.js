3c485158b390e3fda0d90b22e91a956edfc7a6ce
geonhwiii <g01063962671@gmail.com>
2019-09-03 14:08:15 +0900

    Fix almost main UI

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,35 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import { Link as RouterLink } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import useStyles from "../utils/makeStyles";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import List from "@material-ui/core/List";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Hidden from "@material-ui/core/Hidden";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import useStyles from '../utils/makeStyles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import List from '@material-ui/core/List';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
 
 const Home = () => {
   const classes = useStyles();
   const featuredPosts = [
     {
-      title: "Featured post",
-      date: "Nov 12",
-      description:
-        "This is a wider card with supporting text below as a natural lead-in to additional content."
-    },
-    {
-      title: "Featured post",
-      date: "Nov 12",
-      description:
-        "This is a wider card with supporting text below as a natural lead-in to additional content."
-    },
-    {
-      title: "Featured post",
-      date: "Nov 12",
-      description:
-        "This is a wider card with supporting text below as a natural lead-in to additional content."
+      user: '구일모',
+      title: 'CODESTATES DEVELOPER KOO IL MO',
+      subTitle: '안녕하세요 코드스테이츠 DJ를 담당하는 구일모입니다.',
+      date: '2019-09-01',
+      visit: 30,
+      likeCount: 50
     }
   ];
 
@@ -71,7 +61,6 @@ const Home = () => {
         </Toolbar>
 
         <main>
-          {/* Main featured post */}
           <Paper className={classes.mainFeaturedPost}>
             <div className={classes.overlay} />
             <div className={classes.mainFeaturedPostContent}>
@@ -86,94 +75,174 @@ const Home = () => {
             </div>
           </Paper>
         </main>
-        <Grid container spacing={5} className={classes.mainGrid}>
-          <Grid item xs={12} md={2}>
-            <List className={classes.link}>
-              <ListItem button className={classes.linkItem}>
-                <Link
-                  component={RouterLink}
-                  to="/development"
-                  color="inherit"
-                  className={classes.toolbarLink}
-                  underline="none"
-                >
-                  Development
-                </Link>
-              </ListItem>
-              <ListItem button className={classes.linkItem}>
-                <Link
-                  component={RouterLink}
-                  to="/project"
-                  color="inherit"
-                  className={classes.toolbarLink}
-                  underline="none"
-                >
-                  Co-Project
-                </Link>
-              </ListItem>
-              <ListItem button className={classes.linkItem}>
-                <Link
-                  component={RouterLink}
-                  to="/newestit"
-                  color="inherit"
-                  className={classes.toolbarLink}
-                  underline="none"
-                >
-                  Newest IT
-                </Link>
-              </ListItem>
-              <ListItem button className={classes.linkItem}>
-                <Link
-                  component={RouterLink}
-                  to="/hottechissue"
-                  color="inherit"
-                  className={classes.toolbarLink}
-                  underline="none"
-                >
-                  Hot Tech Issue
-                </Link>
-              </ListItem>
-              <ListItem button className={classes.linkItem}>
-                <Link
-                  component={RouterLink}
-                  to="/mycompany"
-                  color="inherit"
-                  className={classes.toolbarLink}
-                  underline="none"
-                >
-                  My Company
-                </Link>
-              </ListItem>
-            </List>
+        <Grid container spacing={8} className={classes.mainGrid}>
+          <Grid item xs={12} md={2} className={classes.categoryGrid}>
+            {/* 카테고리 */}
+            <Grid item>
+              <List className={classes.link}>
+                <ListItem button className={classes.linkItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/development"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    Development
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.linkItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/project"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    Co-Project
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.linkItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/newestit"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    Newest IT
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.linkItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/hottechissue"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    Hot Tech Issue
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.linkItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/mycompany"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    My Company
+                  </Link>
+                </ListItem>
+              </List>
+            </Grid>
+            {/* 태그 */}
+            <Grid item>
+              <List className={classes.tag}>
+                <ListItem button className={classes.tagItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/development"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    #JavaScript
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.tagItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/project"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    #CodeStates
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.tagItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/newestit"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    #Node
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.tagItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/hottechissue"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    #React
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.tagItem}>
+                  <Link
+                    component={RouterLink}
+                    to="/mycompany"
+                    color="inherit"
+                    className={classes.toolbarLink}
+                    underline="none"
+                  >
+                    #Redux
+                  </Link>
+                </ListItem>
+              </List>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={10} className={classes.cardGrid}>
+
+          <Grid item xs={12} md={9} className={classes.cardGrid}>
             {featuredPosts.map(post => (
               <Grid item key={post.title} xs={12} md={12}>
                 <CardActionArea component="a" href="#">
                   <Card className={classes.card}>
+                    <Hidden xsDown>
+                      <Grid item className={classes.cardLeft}>
+                        <Grid item>
+                          <CardMedia
+                            className={classes.cardMedia}
+                            image="https://source.unsplash.com/random"
+                            title="Image title"
+                          />
+                        </Grid>
+                        <Grid item>
+                          <Typography className={classes.cardUser}>
+                            {post.user}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Hidden>
                     <div className={classes.cardDetails}>
-                      <CardContent>
-                        <Typography component="h2" variant="h5">
+                      <CardContent className={classes.cardContent}>
+                        <Typography variant="h4" className={classes.cardTitle}>
                           {post.title}
                         </Typography>
-                        <Typography variant="subtitle1" color="textSecondary">
-                          {post.date}
+                        <Typography
+                          variant="caption"
+                          color="textSecondary"
+                          display="block"
+                          align="right"
+                        >
+                          조회수 {post.visit} 좋아요 {post.likeCount}
                         </Typography>
+
                         <Typography variant="subtitle1" paragraph>
-                          {post.description}
+                          {post.subTitle}
                         </Typography>
-                        <Typography variant="subtitle1" color="primary">
-                          Continue reading...
+
+                        <Typography variant="subtitle2" color="textSecondary">
+                          {post.date}
                         </Typography>
                       </CardContent>
                     </div>
-                    <Hidden xsDown>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="https://source.unsplash.com/random"
-                        title="Image title"
-                      />
-                    </Hidden>
                   </Card>
                 </CardActionArea>
               </Grid>
@@ -184,16 +253,13 @@ const Home = () => {
       {/* Footer */}
       <footer className={classes.footer}>
         <Container maxWidth="lg">
-          <Typography variant="h6" align="center" gutterBottom>
-            Footer
-          </Typography>
           <Typography
             variant="subtitle1"
             align="center"
             color="textSecondary"
             component="p"
           >
-            Something here to give the footer a purpose!
+            made by team Codenation
           </Typography>
         </Container>
       </footer>
--- src/utils/makeStyles.js
+++ src/utils/makeStyles.js
@@ -1,6 +1,7 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
+  // 상단 툴바
   toolbar: {
     borderBottom: `1px solid ${theme.palette.divider}`
   },
@@ -8,32 +9,32 @@ const useStyles = makeStyles(theme => ({
     flex: 1
   },
   toolbarSecondary: {
-    justifyContent: "space-between",
-    overflowX: "auto"
+    justifyContent: 'space-between',
+    overflowX: 'auto'
   },
   toolbarLink: {
     padding: theme.spacing(1),
     flexShrink: 0
   },
   mainFeaturedPost: {
-    position: "relative",
+    position: 'relative',
     backgroundColor: theme.palette.grey[800],
     color: theme.palette.common.white,
     marginBottom: theme.spacing(4),
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center"
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
   },
   overlay: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     bottom: 0,
     right: 0,
     left: 0,
-    backgroundColor: "rgba(0,0,0,.3)"
+    backgroundColor: 'rgba(0,0,0,.3)'
   },
   mainFeaturedPostContent: {
-    position: "relative",
+    position: 'relative',
     padding: theme.spacing(10)
     // margin: theme.spacing(3)
   },
@@ -44,14 +45,38 @@ const useStyles = makeStyles(theme => ({
     // marginBottom: theme.spacing(3)
   },
   card: {
-    display: "flex",
+    display: 'flex',
     marginBottom: theme.spacing(3)
   },
   cardDetails: {
     flex: 1
   },
+  cardLeft: {
+    paddingRight: theme.spacing(2),
+    borderRight: '1px solid rgba(0, 0, 0, 0.05)'
+  },
   cardMedia: {
-    width: 160
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: theme.spacing(4),
+    marginLeft: theme.spacing(2)
+  },
+  cardUser: {
+    textAlign: 'center',
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(2),
+    padding: theme.spacing(0.2),
+    border: 'none',
+    borderRadius: '8px',
+    background: 'rgba(0, 0, 0, 0.05)'
+  },
+  cardContent: {
+    margin: theme.spacing(2)
+  },
+  cardTitle: {
+    fontSize: '2rem',
+    paddingBottom: theme.spacing(3)
   },
   markdown: {
     ...theme.typography.body2,
@@ -69,11 +94,29 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(8),
     padding: theme.spacing(6, 0)
   },
+  categoryGrid: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(4)
+  },
   link: {
-    width: 150
+    width: 170,
+    borderBottom: `1px solid ${theme.palette.divider}`
   },
   linkItem: {
-    marginBottom: theme.spacing(6)
+    fontSize: '1.2rem',
+    fontWeight: 600,
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3)
+  },
+  tag: {
+    width: 170
+  },
+  tagItem: {
+    color: 'lightgray',
+    background: 'white',
+    marginTop: theme.spacing(3),
+    boxShadow: `0 3px 3px rgba(0, 0, 0, 0.3)`,
+    borderRadius: '20px'
   }
 }));
 
