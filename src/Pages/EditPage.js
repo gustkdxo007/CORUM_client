0895282d8decb4a69ca4a876e9609c6a6c1a7273
geonhwiii <g01063962671@gmail.com>
2019-09-03 23:49:24 +0900

    Make save & submit button & category select

--- src/Pages/EditPage.js
+++ src/Pages/EditPage.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Container, Grid, CssBaseline, TextField } from '@material-ui/core';
+import {
+  Container,
+  Grid,
+  CssBaseline,
+  TextField,
+  Button,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput
+} from '@material-ui/core';
 import useStyles from '../utils/editStyles';
 import IntegrationReactSelect from '../utils/selectHelper';
 
 const EditPage = () => {
   const classes = useStyles();
+  const [values, setValues] = React.useState({
+    category: ''
+  });
 
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+  function handleChange(event) {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value
+    }));
+  }
   return (
     <div className={classes.root}>
       <Container maxWidth="lg">
@@ -13,6 +39,7 @@ const EditPage = () => {
         <Grid container spacing={0}>
           <Grid item xs={12} md={9}>
             <form className={classes.form} noValidate>
+              {/* Title */}
               <TextField
                 variant="standard"
                 margin="normal"
@@ -23,6 +50,7 @@ const EditPage = () => {
                 name="title"
                 autoFocus
               />
+              {/* Subtitle */}
               <TextField
                 variant="standard"
                 margin="normal"
@@ -33,12 +61,55 @@ const EditPage = () => {
                 label="SubTitle"
                 type="subTitle"
               />
-              {/* 태그 들어갈 자리 */}
+              {/* Tags Select */}
               <IntegrationReactSelect />
             </form>
           </Grid>
           <Grid item xs={12} md={3}>
-            DEF
+            {/* form with button & select */}
+            <form className={classes.root} autoComplete="off">
+              {/* 작성하기 버튼 */}
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                fullWidth
+              >
+                작성하기
+              </Button>
+              {/* 임시저장 버튼 */}
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.saveButton}
+                fullWidth
+              >
+                저장하기
+              </Button>
+              {/* 카테고리 Select */}
+              <FormControl
+                fullWidth
+                variant="outlined"
+                className={classes.formControl}
+              >
+                <InputLabel className={classes.inputLabel} ref={inputLabel}>
+                  Category
+                </InputLabel>
+                <Select
+                  value={values.category}
+                  onChange={handleChange}
+                  input={
+                    <OutlinedInput labelWidth={labelWidth} name="category" />
+                  }
+                >
+                  <MenuItem value={'Developer'}>Developer</MenuItem>
+                  <MenuItem value={'Co-project'}>Co-project</MenuItem>
+                  <MenuItem value={'Newest IT'}>Newest IT</MenuItem>
+                  <MenuItem value={'Hot Tech Issue'}>Hot Tech Issue</MenuItem>
+                  <MenuItem value={'My Company'}>My Company</MenuItem>
+                </Select>
+              </FormControl>
+            </form>
           </Grid>
         </Grid>
       </Container>
--- src/utils/editStyles.js
+++ src/utils/editStyles.js
@@ -3,6 +3,24 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
+  },
+  submitButton: {
+    margin: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    fontSize: '1.3rem',
+    fontWeight: '600'
+  },
+  formControl: {
+    margin: theme.spacing(2, 1)
+  },
+  saveButton: {
+    margin: theme.spacing(1.5, 1),
+    marginTop: theme.spacing(2),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    fontSize: '1.3rem',
+    fontWeight: '600'
   }
 }));
 
