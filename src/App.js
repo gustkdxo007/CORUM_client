d66f583e44c331f9d2e382e104cc6d9702c42f28
KimDonguk <gustkxo007@gmail.com>
2019-09-04 14:47:02 +0900

    make editpage routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
+import EditPage from "./Pages/EditPage";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/edit" component={EditPage} />
       </Switch>
     </Router>
   );
